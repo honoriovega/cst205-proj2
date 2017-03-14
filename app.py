6473cfcd671df440b7c28bbb884cf94d69f1366e
Honorio Vega <hvega@csumb.edu>
2017-03-14 00:25:05 +0000

    spotify

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def spotify(data):
 	searchQuery1 = searchQuery.replace("+", "%20")
 	response = requests.get("https://api.spotify.com/v1/search?q="+searchQuery1+"&type="+searchType)
 	json = response.json()
-	print json['items']['uri']
+	print json[0]['items']['uri']
 	idholder =  []
 	idholder = [URI['uri'] for URI in json['uri'] ]
 	print idholder
