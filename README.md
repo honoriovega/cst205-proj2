bfd4a87995f6baae24bc7a7732a247cc117a5758
Honorio Vega <hvega@csumb.edu>
2017-03-16 14:51:39 +0000

    readme update

--- README.md
+++ README.md
@@ -2,23 +2,24 @@
 ## Authors: Javar Alexander, Honorio Vega, Antonio Villagomez
 ## CST 205
 ## 03/16/2016
-## to run the program, go here: <a href="https://cst205-project2.herokuapp.com/">https://cst205-project2.herokuapp.com/</a>
+## to run the program, go here: <a href="https://cst205-project2.herokuapp.com/">https://cst205-project2.herokuapp.com/</a> click sign in,
+you can authenticate with either facebook or google. 
+
 ## github repo <a href="https://github.com/honoriovega/cst205-proj2">https://github.com/honoriovega/cst205-proj2</a>
 ## Project Desciption
+
 
 ### For this project, we decided to make a web app where users can login and chat with one another. We added external apis such as
 Getty images for dynamic background images, spotify for our group music player. We also implemented giffy so users can send gifs in chat. 
 Think of this as basically the structure for a web based slack clone. There is a bot that the user can interact with. 
+
+## Future work
+### We think it'll be cool if we were able to fully implement the youtube serch api with visual results. Also we bounced around the idea
+with implementing socketio video chat with the python backend and client side in JS.
 
 ## Who worked on what? 
 ### Javar and Honorio worked on server side, setting up the chat functionality and bot commands, ReactJS, and Spotify integration. 
 ### Antonio focused more on client side views (CSS). 
 
 
-Name of project
-Names of team members
-Class
-Date
-How to run program
-Link to GitHub repository
-Future work
+
--- app.py
+++ app.py
@@ -9,12 +9,10 @@ import json
 
 app = flask.Flask(__name__)
 
-
 #app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'postgresql://potato:potatosareawesome@localhost/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = flask_sqlalchemy.SQLAlchemy(app)
-
 
 class Message(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -23,17 +21,14 @@ class Message(db.Model):
 	name = db.Column(db.String(100))
 	apiLink = db.Column(db.String(500))
 
-	
 	def __init__(self, p,n,t,al=''):
 		self.text = t
 		self.picture = p
 		self.name = n
 		self.apiLink = al
-	
+
 	def __repr__(self):
 		return '<%s %s: %s>' % (self.picture, self.name, self.text)
-
-
 
 socketio = flask_socketio.SocketIO(app)
 
@@ -67,23 +62,21 @@ def addMessage(userPicture, name, msg):
 	db.session.add(message)
 	db.session.commit()
 
-
 def addBotMessage(msg):
 	BOT_PICTURE = '/static/bot.jpg'
 	BOT_NAME = 'Bender_from_futurama'
 	addMessage(BOT_PICTURE,BOT_NAME,msg)
-	
+
 def addBotMessageAPI(link):
 	BOT_PICTURE = '/static/bot.jpg'
 	BOT_NAME = 'Bender_from_futurama'
 	addPictureMessage(BOT_PICTURE,BOT_NAME,link)
 
-
 def addPictureMessage(userPicture, name, apiLink):
 	message = Message(userPicture,name, '', apiLink)
 	db.session.add(message)
 	db.session.commit()
-	
+
 @app.route('/')
 def hello():
 	keywords = ['technology','forest','background']
@@ -93,57 +86,15 @@ def hello():
 
 @socketio.on('connect')
 def on_connect():
-	"""
-	name = random.randrange(1000, 9999)
-
-	flask_socketio.emit(
-		'server generated a new name',
-		{
-			'name': name
-		}
-	)
-
-	all_connected_users[flask.request.sid] = name
-
-	socketio.emit(
-		'list of all users',
-		{
-			'users': all_connected_users.values()
-		}
-	)
-	
-	
-
-	
-	greet = "Welcome to the chatroom new user. Type !! help to view list of commands"
-	addBotMessage(greet)
-	"""
 	fetchAndEmit()
-
 
 @socketio.on('new number')
 def on_new_number(data):
-	"""
-	response = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + data['google_user_token'])
-	json = response.json()
-
-	all_numbers.append({
-	'name': json['name'],
-	'picture': json['picture'],
-	'number': data['number']
-	})
-
-	socketio.emit('all numbers', {
-		'numbers': all_numbers
-	})
-	"""
 	all_numbers.append(100)
 	socketio.emit('all numbers', {
 			'numbers' : all_numbers
 	})
-	
-	
-	
+
 @socketio.on('Spotify')
 def spotify(data):
 	tracks =[]
@@ -156,46 +107,43 @@ def spotify(data):
 		for item in json['tracks']['items']:
 			print item['uri']
     		tracks.append(item['uri'])
-        	
+
 	my_headers = {"Accept" : "application/json", "Authorization" : "Bearer BQCm9bzjiDxNb9FurI8AWVgraOhvdZyzpBBNq753DwEXocrLa8kyPNOalfXuevtiZ10Kt8FIuvM1RMnv6mWiVsz9bXU8VQzEv3xdHAE5Qs4-eFI4dh3spBArHnzQLl6gGqvddte-H7JZQVzJEsxobx1TSStfVqonFzxWdH418b5RtzgZMHFgnKtV-6qW9g_axQ1bKwQ4Fm8e1NI"}
 	url = "https://api.spotify.com/v1/tracks/1zHlj4dQ8ZAtrayhuDDmkY"
 	track_response = requests.get(url, headers= my_headers)
 	spotify_links = track_response.json()
-	print spotify_links
 	random_track = random.choice(tracks)
-	
+
 	random_track_link = "https://embed.spotify.com/?uri="+random_track
 	socketio.emit('fromSpotify', random_track_link)
-	
-	
-	
+
 @socketio.on('greet user')
 def greet_user(data):
 	picture = ''
 	USERNAME = ''
 	greet = ''
-	
+
 	if(data['google_user_token'] == '' and data['facebook_user_token'] == ''):
 		send('greeting user')
-	
+
 	else:
-	
+
 		if(data['google_user_token'] == ''):
 			response = requests.get('https://graph.facebook.com/v2.8/me?fields=id%2Cname%2Cpicture&access_token=' + data['facebook_user_token'])
 			json = response.json()
 			USERNAME =  json['name']
 			picture =  json['picture']['data']['url']
 			greet = 'Hello ' + USERNAME + ' logged in from Facebook'
-	
+
 		else:
 			response = requests.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + data['google_user_token'])
 			json = response.json()
 			picture = json['picture']
 			USERNAME = json['name']
 			greet = 'Hello ' + USERNAME + ' logged in from Google'
-	
+
 		addBotMessage(greet)
-	
+
 		fetchAndEmit()
 
 @socketio.on('new msg')
@@ -209,45 +157,31 @@ def on_new_msg(data):
 	msg = msg.strip()
 	if(data['google_user_token'] == '' and data['facebook_user_token'] == '' ):
 		send('received message')
-	
-	
+
 	else:
 		if(data['google_user_token'] == ''):
 			response = requests.get( facebookAPI + data['facebook_user_token'])
 			json = response.json()
 			USERNAME =  json['name']
 			picture = json['picture']['data']['url']
-	
+
 		else:
 			response = requests.get(googleAPI + data['google_user_token'])
 			json = response.json()
 			picture = json['picture']
 			USERNAME = json['name']
-		
-
 
 	url = msg
 	parts = urlparse.urlsplit(url)
-	
+
 	# it is not a url so add it and emit
-	if not parts.scheme or not parts.netloc:  
-		"""
-		if(len(msg) > 4):
-			if(msg[len(msg) - 3:] in ['edu','com','net','gov','org']):
-				addPictureMessage(picture,USERNAME,'http://' +msg)
-			else:
-				addMessage(picture,USERNAME,  msg)
-		else:
-			addMessage(picture,USERNAME, msg)
-		"""
-		
+	if not parts.scheme or not parts.netloc:
+
 		if('!! say' in msg):
 			x = 10
 		else:
 			addMessage(picture,USERNAME, msg)
-		
-		
-		
+
 	else:
 		print "yes an url"
 		addPictureMessage(picture,USERNAME,url)
@@ -262,7 +196,6 @@ def on_new_msg(data):
 				addBotMessage(response)
 		else:
 			addBotMessage(response)
-	
 
 	fetchAndEmit()
 if __name__ == '__main__':
